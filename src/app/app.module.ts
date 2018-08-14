40f21ced135cca097be4631d727a4f283335d067
Konrad Wysk <konrad.wysk@gmail.com>
2018-08-14 12:19:40 +0200

    Ajout du composant header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsListComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule
