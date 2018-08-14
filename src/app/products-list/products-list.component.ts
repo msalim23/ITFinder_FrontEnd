ad21c9618d1c289f8cfc5d9b5ab7c8a6b666c6d7
Konrad Wysk <konrad.wysk@gmail.com>
2018-08-14 14:10:49 +0200

    Ajout du composant produits list

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
+  data: any[];
+
   constructor() { }
 
   ngOnInit() {
+
+    this.data = [
+      {'code': 'P100', 'titre': 'Cafe', 'prixUnitaire': 5.5},
+      {'code': 'P200', 'titre': 'The', 'prixUnitaire': 4.5},
+      {'code': 'P300', 'titre': 'Jus', 'prixUnitaire': 6}
+    ];
   }
 
 }
